33e347cb221196e3494ed5231445193f5c8c4465
RoqayaMohammad <roqayamohammadrayan@gmail.com>
2023-09-19 01:58:02 +0300

    adding models and DbContext

---
+++ Expense Tracker/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options) 
+        {
+                
+        }
+        public DbSet<Transaction> Transaction { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
