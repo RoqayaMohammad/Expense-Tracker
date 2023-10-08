298908a258700f01cfcd99a2e39968c5e1865a9d
RoqayaMohammad <roqayamohammadrayan@gmail.com>
2023-10-09 00:20:14 +0300

    Caegories page

--- Expense Tracker/Controllers/CategoriesController.cs
+++ Expense Tracker/Controllers/CategoriesController.cs
@@ -73,23 +73,8 @@ namespace Expense_Tracker.Controllers
 
        
 
-        // GET: Categories/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Category == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Category
-                .FirstOrDefaultAsync(m => m.CategoryId == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
+        
+        
 
         // POST: Categories/Delete/5
         [HttpPost, ActionName("Delete")]
